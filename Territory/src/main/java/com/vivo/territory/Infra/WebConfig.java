96ccc661ded6fb4ea26d24f825a4d7fcfe7b16d2
Vinicius <orsivini@outlook.com>
2024-10-21 11:19:09 -0300

    configurando cord

---
+++ Territory/src/main/java/com/vivo/territory/Infra/WebConfig.java
@@ -0,0 +1,18 @@
+package com.vivo.territory.Infra;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Permite CORS para todas as rotas
+                .allowedOrigins("http://localhost:4200") // Permite apenas a origem do seu frontend
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                .allowedHeaders("*") // Cabeçalhos permitidos
+                .allowCredentials(true); // Permitir credenciais (se necessário)
+    }
+}
